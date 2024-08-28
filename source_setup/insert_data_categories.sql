8aac8570f26728cf49abf20b3e9970b8506e1cc9
Hasbullah Abu Bakar <mhasbullahab@bankislam.com.my>
2024-08-28 09:04:12 +0800

    setup database objects and  data

---
+++ source_setup/insert_data_categories.sql
@@ -0,0 +1,17 @@
+
+
+
+
+
+-- Insert sample data into Categories table
+INSERT INTO Categories (Category_Name)
+VALUES ('Electronics'),
+       ('Computers'),
+       ('Home Appliances'),
+       ('Books'),
+       ('Clothing'),
+       ('Toys'),
+       ('Beauty'),
+       ('Sports'),
+       ('Automotive'),
+       ('Health');
